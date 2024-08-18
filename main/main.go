baa67f98b035e70bda5cb8cc824c7a39c4a509e2
agenthx <87952665+agenthx@users.noreply.github.com>
2024-08-18 15:36:07 +0000

    f

--- main/main.go
+++ main/main.go
@@ -1,4 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
+	filee, err := os.ReadFile("standard.txt")
+
+	file := string(filee)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+  ascii:= '!'
+
+  count := 0
+	str := strings.Split(file, "\n")
+	for i := 1; i < len(str); i++ {
+
+		if i%9 == 0 {
+			fmt.Println()
+		} else {
+      if i%8 == 0{
+        count++
+      }
+      
+			fmt.Println(str[i])
+		}
+	}
+  //make a loop that prints the first line to the 8th line of letters together
+  
+  for i := 0; i < len(str); i++ {
+    
   }
+
+
+}
