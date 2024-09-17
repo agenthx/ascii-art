048d38e87426165c2bd7261201d5b74f6c84adcd
agenthx <87952665+agenthx@users.noreply.github.com>
2024-09-17 15:07:13 +0000

    Add files via upload

---
+++ functions/AsciiFCOJ.go
@@ -0,0 +1,149 @@
+package piscine
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
+
+func AsciiFCOJ(str []string, input, sub, color, fileN, align string) {
+	// find if fileN is there
+	var file *os.File
+	if fileN != "" {
+		file, _ = os.Create(fileN)
+	}
+	// if input is empty return nothing
+	if input == "" {
+		if fileN != "" {
+			file.WriteString("")
+		}
+		return
+	}
+	// check if color string is empty if not do case to match it
+	var flag bool
+	var reset string
+	if color != "" {
+		flag = true
+		reset = "\033[0m"
+
+		switch color {
+		case "gray":
+			color = "\033[30m"
+		case "red":
+			color = "\033[31m"
+		case "green":
+			color = "\033[32m"
+		case "yellow":
+			color = "\033[33m"
+		case "blue":
+			color = "\033[34m"
+		case "purple":
+			color = "\033[35m"
+		case "cyan":
+			color = "\033[36m"
+		case "white":
+			color = "\033[37m"
+		case "orange":
+			color = "\033[38;5;208m"
+		default:
+			fmt.Println("invalid color")
+			os.Exit(3)
+		}
+
+	}
+	// check if align string is not empty
+	if align != "" { //if an align was specified do this
+		switch align {
+		case "right": 
+			//if it was right
+		case "left":
+			// do this
+		case "center":
+			// do this
+		case "justify":
+			// do this
+		}
+	}
+	// Replace all "\n" in the input string with an actual newline
+	input2 := strings.ReplaceAll(input, "\\n", "\n")
+	// check if the input consists of only \n
+	reN := regexp.MustCompile(`.`)
+	if !reN.MatchString(input2) {
+		if fileN != "" {
+			file.WriteString(input2)
+		} else {
+			fmt.Print(input2)
+		}
+		return
+	}
+	// Split the input string into lines
+	lines := strings.Split(input2, "\n")
+	// for sub things
+	var sub2 []string
+	var indOfSub [][]int
+	if flag && sub != "" {
+		sub = strings.ReplaceAll(sub, "\\n", "\n")
+		sub2 = strings.Split(sub, "\n")
+	}
+	// Iterate over the lines
+	for s, line := range lines {
+		if line == "" {
+			fmt.Println()
+		} else {
+			// find where is the substring in the following line
+			if flag && sub2 != nil && len(sub2) != 1 {
+				reI := regexp.MustCompile(sub2[s])
+				indOfSub = reI.FindAllStringIndex(line, -1)
+			} else if flag && sub2 != nil && len(sub2) > 1 {
+				reI := regexp.MustCompile(sub2[0])
+				indOfSub = reI.FindAllStringIndex(line, -1)
+			} else if flag {
+				sub = strings.ReplaceAll(sub, "+", "\\+")
+				sub = strings.ReplaceAll(sub, "$", "\\$")
+				sub = strings.ReplaceAll(sub, "^", "\\^")
+				sub = strings.ReplaceAll(sub, "*", "\\*")
+				sub = strings.ReplaceAll(sub, "(", "\\(")
+				sub = strings.ReplaceAll(sub, ")", "\\)")
+				reI := regexp.MustCompile(sub)
+				indOfSub = reI.FindAllStringIndex(line, -1)
+			}
+			// Create a slice to store the lines for the current row
+			var lineSlice string
+			// Iterate over the 8 rows for each line
+			for i := 0; i < 8; i++ {
+				// Iterate over the characters in the current line
+				for j, ch := range line {
+					// Check if the character is a valid ASCII character
+					if ch < 32 || ch > 128 {
+						fmt.Println("error input")
+						return
+					}
+					// Calculate the line index for the current character
+					lineIndex := 1 + int(ch-32)*9 + i
+					// Append the corresponding string to the line slice
+					if ContSub(indOfSub, j) {
+						lineSlice += color + str[lineIndex] + reset
+					} else {
+						lineSlice += str[lineIndex]
+					}
+				}
+				// Join the lines in the slice and print the row
+				if fileN != "" {
+					file.WriteString(lineSlice + "\n")
+				} else {
+					// lenOfS
+					var kiki string
+					spaces := width() - len(lineSlice)
+					// fmt.Println(spaces,lt,len(lineSlice))
+					for i := 1; i < spaces; i++ {
+						kiki += " "
+					}
+					fmt.Println(kiki,lineSlice)
+				}
+				// Clear the line slice for the next row
+				lineSlice = ""
+			}
+		}
+	}
+}
---
+++ main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	piscine "piscine/functions"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	//check if the arguments given are arguments are not too little or much
+	if len(os.Args) < 2 || len(os.Args) > 5 {
+		fmt.Println("error: no.of arguments")
+		return
+	}
+	banner := "standard.txt"
+	var color string
+	var sub string
+	var input string
+	var fileN string
+	var align string
+	b4Last := os.Args[len(os.Args)-2]
+	lastArg := os.Args[len(os.Args)-1]
+	//find the banner
+	reF := regexp.MustCompile(`(^standard$|^shadow$|^thinkertoy$)`)
+	//find if file is txt
+	reFF := regexp.MustCompile(`(^standard\.txt$|^shadow\.txt$|^thinkertoy\.txt$)`)
+	//find if align is valid
+	reA := regexp.MustCompile(`center|left|right|justify`)
+	//if the argument starts with color flag
+	if strings.Contains(os.Args[1], "--color=") {
+		//store the color name
+		color = strings.TrimPrefix(os.Args[1], "--color=")
+		//if the last arg is a banner then b4 it is the string
+		if reFF.MatchString(lastArg) || reF.MatchString(lastArg) {
+			if reFF.MatchString(lastArg) {
+				banner = lastArg
+			} else {
+				banner = lastArg + ".txt"
+			}
+			input = b4Last
+		} else {
+			if len(os.Args) >= 5 {
+				fmt.Println("Error: arguments given are incorrect")
+				os.Exit(1)
+			}
+			input = lastArg
+		}
+		//if arg 2 is differnt than input put it as sub
+		if input == os.Args[2] {
+			sub = input
+		} else {
+			sub = os.Args[2]
+		}
+	} else if strings.Contains(os.Args[1], "--output=") {
+		if len(os.Args) == 2 {
+			fmt.Println("eror: no-of args")
+			return
+		}
+		fileN = strings.TrimPrefix(os.Args[1], "--output=")
+		if reF.MatchString(fileN) || reFF.MatchString(fileN) {
+			fmt.Println("Error: File name invalid!")
+			os.Exit(3)
+		}
+		input = os.Args[2]
+	} else if strings.Contains(os.Args[1], "--align=") {
+		align = strings.TrimPrefix(os.Args[1], "--align=") //where to align text
+		//check if the align is valid
+		if !reA.MatchString(align) {
+			fmt.Println("Error: aligns are: center, left, right, justify")
+			os.Exit(5)
+		}
+		input = os.Args[2]
+	} else {
+		if len(os.Args) >= 4 {
+			fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]\nExample: go run . --output=<fileName.txt> something standard")
+			os.Exit(2)
+		}
+		input = os.Args[1]
+		if len(os.Args) > 2 {
+			if reFF.MatchString(os.Args[2]) {
+				banner = os.Args[2]
+			} else if reF.MatchString(lastArg) {
+				banner = os.Args[2] + ".txt"
+			} else {
+				fmt.Println("banner name incorrect")
+				os.Exit(4)
+			}
+		}
+	}
+	//for banners
+	if len(os.Args) == 4 {
+		if reFF.MatchString(os.Args[3]) {
+			banner = os.Args[3]
+		} else if reF.MatchString(os.Args[3]) {
+			banner = os.Args[3] + ".txt"
+		} else {
+			fmt.Println("banner name incorrect")
+			os.Exit(4)
+		}
+	}
+	//open and store the contents of the standard.txt file
+	file, err := os.ReadFile("banner/" + banner)
+	if err != nil {
+		fmt.Print(err)
+	}
+	//split the contents of standard.txt file into line by line and store each line in a slice in the array
+	str := strings.Split(string(file), "\n")
+	//give the input and str into PrintASCII function
+	piscine.AsciiFCOJ(str, input, sub, color, fileN, align)
+}
